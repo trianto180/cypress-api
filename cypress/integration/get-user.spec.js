c82f2899c8f7c5da69b8d155fac924a75e30b63b
Angga <angga.trianto@students.amikom.ac.id>
2022-06-07 01:47:49 +0700

    get user

---
+++ cypress/integration/get-user.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Get User', ()=>{
+    it('List user', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://reqres.in/api/users?page=2'
+        }).as('users')
+        cy.get('@users').its('status').should('eq', 200)
+        cy.get('@users').its('headers').its('content-type').should('include', 'application/json; charset=utf-8')
+    });
+});
