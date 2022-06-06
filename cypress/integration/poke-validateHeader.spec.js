61ebc8665aba5c43dd30b36a97af61e474514818
Angga <angga.trianto@students.amikom.ac.id>
2022-06-07 03:49:42 +0700

    validate status

--- cypress/integration/poke-validateHeader.spec.js
+++ cypress/integration/poke-validateHeader.spec.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 describe('validate header', ()=>{
     it('Validate header', () => {
         var ability = {
@@ -9,5 +11,18 @@ describe('validate header', ()=>{
             expect(response.body.name).to.eq(ability.name)
         })
         cy.get('@pokeapi').its('headers').its('content-type').should('include', 'application/json; charset=utf-8')
+    });
+
+    it('Validate status code', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('ditto')
+        cy.get('@ditto').its('status').should('equal', 200)
+    });
+
+    it('Validate status code with params', () => {
+        cy.request({
+            method: "GET",
+            url: 'https://reqres.in/api/users?page=2'
+        }).as('users')
+        cy.get('@users').its('status').should('equal', 200)
     });
 });
